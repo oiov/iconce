46b1926cbac0e20d96088832800b5f7b542958a8
yesmore <3224266014@qq.com>
2024-01-29 11:27:47 +0800

    upd

--- components/MainHeader.tsx
+++ components/MainHeader.tsx
@@ -1,16 +1,10 @@
-import Image from "next/image";
+import LogoSVG from "@/components/icons/LogoSVG";
 import Link from "next/link";
 
 export default function MainHeader() {
   return (
     <Link href="/" className="flex space-x-2 items-center">
-      <Image
-        alt="header text"
-        src="/ce.svg"
-        className="w-8 h-8"
-        width="16"
-        height="16"
-      />
+      <LogoSVG className="w-8 h-8" />
       <h1 className="text-xl md:text-2xl font-[900] text-gradient-1 flex items-center">
         𝐈𝐂𝐎𝐍𝐂𝐄
       </h1>
---
+++ components/icons/LogoSVG.tsx
@@ -0,0 +1,56 @@
+export default function LogoSVG({
+  className,
+  onClick,
+}: {
+  className?: string;
+  onClick?: () => void;
+}) {
+  return (
+    <svg
+      className={className}
+      id="iconce-svg"
+      width="256"
+      height="256"
+      viewBox="0 0 256 256"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink">
+      <rect
+        id="r4"
+        width="256"
+        height="256"
+        x="0"
+        y="0"
+        rx="64"
+        fill="url(#r5)"
+        stroke="#FFFFFF"
+        stroke-width="0"
+        stroke-opacity="100%"
+        paint-order="stroke"
+      />
+      <clipPath id="clip">
+        <use xlinkHref="#r4" />
+      </clipPath>
+      <defs>
+        <linearGradient
+          id="r5"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="rotate(45)"
+          style={{ transformOrigin: "center center" }}>
+          <stop stop-color="#8E2DE2" />
+          <stop offset="1" stop-color="#4A00E0" />
+        </linearGradient>
+        <radialGradient
+          id="r6"
+          cx="0"
+          cy="0"
+          r="1"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="translate(256) rotate(90) scale(512)">
+          <stop stop-color="white" />
+          <stop offset="1" stop-color="white" stop-opacity="0" />
+        </radialGradient>
+      </defs>
+    </svg>
+  );
+}
