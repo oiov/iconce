b7d32ce7c23bc2ff60a2db247b97a69173073d16
yesmore <3224266014@qq.com>
2024-02-03 10:14:25 +0800

    upd

--- components/MainHeader.tsx
+++ components/MainHeader.tsx
@@ -5,7 +5,9 @@ export default function MainHeader() {
   return (
     <Link href="/" className="flex space-x-2 items-center">
       <LogoSVG className="w-8 h-8" />
-      <h1 className="text-xl md:text-2xl font-[900] text-gradient-1 flex items-center">
+      <h1
+        className="text-xl md:text-2xl font-[900] text-gradient-1 flex items-center"
+        style={{ visibility: "hidden" }}>
         ICONCE
       </h1>
     </Link>
